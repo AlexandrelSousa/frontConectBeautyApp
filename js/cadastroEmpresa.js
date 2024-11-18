e8b4dc7c6e825127967224d71415cba4feff4d2f
Alexandrelsousa <alexandrelsousa@outlook.com>
2024-11-18 21:59:56 -0200

    alterando a rota para cadastro de empresa.

--- js/cadastroEmpresa.js
+++ js/cadastroEmpresa.js
@@ -1,3 +1,5 @@
+const URLAPI = "https://api-conect-beauty.vercel.app"
+
 function nextForm1(){
     document.getElementById("form1").style.display = "none";
     document.getElementById("form2").style.display = "flex";
@@ -82,59 +84,6 @@ function validarCNPJ(cnpj) {
     return true;
 }
 
-/*function registerEmpresa(){
-    const inputFiles = document.getElementById("uploadImage").files[0];
-    if(validarCNPJ(document.getElementById("cnpj").value)){
-        if(inputFiles){
-            const dias = [document.getElementById("domingo").checked, document.getElementById("segunda").checked, document.getElementById("terça").checked, document.getElementById("quarta").checked, document.getElementById("quinta").checked, document.getElementById("sexta").checked, document.getElementById("sábado").checked]
-            const formData = new formData();
-            const empresaData = {
-              cnpj: document.getElementById("cnpj").value,
-              senha: document.getElementById("senha").value,
-              nome: document.getElementById("nome").value,
-              telefone: document.getElementById("telefone").value,
-              cidade: document.getElementById("cidade").value,
-              bairro: document.getElementById("bairro").value,
-              logradouro: document.getElementById("logradouro").value,
-              numero: document.getElementById("numero").value,
-              descricao: document.getElementById("descrição").value,
-              classificacao: "0",
-              inicio_expediente: document.getElementById("horaInicial").value,
-              fim_expediente: document.getElementById("horaFinal").value,
-              dias_func: dias
-            };
-            fetch('http://localhost:3030/empresa/cadastrar', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(empresaData)
-            })
-            .then(response => {
-                if (!response.ok) {
-                    // Se a resposta não estiver OK, exibe a mensagem de erro diretamente
-                    return response.text().then(errorMessage => {
-                        throw new Error('Erro durante o cadastro da empresa! ' + errorMessage);
-                    });
-                }
-                return response.text();
-            })
-            .then(data => {
-                console.log('Empresa cadastrada com sucesso:', data);
-                alert('Empresa cadastrada com sucesso!');
-                window.location.href = '../index.html';
-            })
-            .catch(error => {
-                // Captura e exibe o erro no console
-                console.error('Erro durante a requisição:', error.message);
-                alert('Erro durante a requisição: ' + error.message);
-            });
-        }
-    }else{
-        alert("CNPJ inválido")
-    }
-}*/
-
 function registerEmpresa() {
     const inputFiles = document.getElementById("uploadImage").files[0];
 
@@ -177,7 +126,7 @@ function registerEmpresa() {
         }
 
         // Faz a requisição POST com o FormData
-        fetch('http://localhost:3030/empresa/cadastrar', {
+        fetch(`${URLAPI}/api/empresa/cadastrar`, {
             method: 'POST',
             body: formData // Envia o FormData com todos os dados e arquivos
         })
